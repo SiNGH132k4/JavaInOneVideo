3bc791009991c7a48e81ca990ef12d264a817f8a
Shivam Singh <ss132k4@gmail.com>
2025-03-27 00:16:42 +0530

    Adding Java in 1 video

--- src/com/singh/javain1video/conditionals/Loops.java
+++ src/com/singh/javain1video/conditionals/Loops.java
@@ -3,5 +3,25 @@ package com.singh.javain1video.conditionals;
 public class Loops {
     public static void main(String[] args) {
 
+        // WHILE LOOP
+        int i = 0;
+        while (i<=100){
+            System.out.println("Hello " + i);
+            i++;
+        }
+
+        // FOR LOOP
+        for (int j=0; j<=100; j++){
+            System.out.println("Heyy " + j);
+        }
+
+        // DO WHILE LOOP
+        int k = 101;
+
+        do {
+            System.out.println(k);
+            k++;
+        }while (k<=100);
+
     }
 }
