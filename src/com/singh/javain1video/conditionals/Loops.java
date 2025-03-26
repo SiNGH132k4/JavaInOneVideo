59717a827c0235592433e8833280aa73bf39bcc6
Shivam Singh <ss132k4@gmail.com>
2025-03-26 23:50:17 +0530

    Adding Java in 1 video

---
+++ src/com/singh/javain1video/conditionals/Loops.java
@@ -0,0 +1,7 @@
+package com.singh.javain1video.conditionals;
+
+public class Loops {
+    public static void main(String[] args) {
+
+    }
+}
--- src/com/singh/javain1video/conditionals/Practice.java
+++ src/com/singh/javain1video/conditionals/Practice.java
@@ -2,11 +2,79 @@ package com.singh.javain1video.conditionals;
 
 public class Practice {
     public static void main(String[] args) {
-        boolean isSunny = true;
-        boolean isWarm = true;
 
-        if (isSunny && isWarm){
-            System.out.println("Beach day");
-        }
+        // TERNARY OPERATOR
+        // result = (condition) ? valueIfTrue : valueIfFalse
+        int a = 1;
+        String str = a % 2 == 0 ? "isEven" : "isOdd";
+        System.out.println(str);
+
+        // SWITCH STATEMENT
+//        int day = 3;
+//        String dayName;
+//
+//        switch (day) {
+//            case 1:
+//                dayName= "Monday";
+//                break;
+//            case 2:
+//                dayName= "Tuesday";
+//                break;
+//            case 3:
+//                dayName= "Wednesday";
+//                break;
+//            case 4:
+//                dayName= "Thursday";
+//                break;
+//            case 5:
+//                dayName= "Friday";
+//                break;
+//            case 6:
+//                dayName= "Saturday";
+//                break;
+//            case 7:
+//                dayName= "Sunday";
+//                break;
+//            default:
+//                dayName= "Invalid day";
+//        }
+//        System.out.println(dayName);
+
+        // IF-ELSE LADDER
+//        int day = 9;
+//        String dayName;
+//
+//        if (day == 1) {
+//            dayName = "Monday";
+//        } else if (day == 2) {
+//            dayName = "Tuesday";
+//        }else if (day == 3) {
+//            dayName = "Wednesday";
+//        }else if (day == 4) {
+//            dayName = "Thursday";
+//        }else if (day == 5) {
+//            dayName = "Friday";
+//        }else if (day == 6) {
+//            dayName = "Saturday";
+//        }else if (day == 7) {
+//            dayName = "Sunday";
+//        } else {
+//            dayName = "Choose right number";
+//        }
+//
+//        System.out.println(dayName);
+
+//      IF-ELSE
+//        boolean isSunny = true;
+//        boolean isWarm = true;
+
+//        if (isSunny && !isWarm){
+//            System.out.println("Beach day");
+//        } else if(isSunny){
+//            System.out.println("Put something on");
+//        }
+//        else {
+//            System.out.println("Stay at home");
+//        }
     }
 }
