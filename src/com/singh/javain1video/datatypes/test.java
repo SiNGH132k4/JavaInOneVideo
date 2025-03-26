feb89a2a147a4f7852dd598853124b8ab6af9b7b
Shivam Singh <ss132k4@gmail.com>
2025-03-26 17:28:30 +0530

    Adding Java in 1 video

---
+++ src/com/singh/javain1video/datatypes/test.java
@@ -0,0 +1,12 @@
+package com.singh.javain1video.datatypes;
+
+public class test {
+    public static void main(String[] args) {
+        int a = 1;
+        byte b = 2;
+        short c = 3;
+        long d = 4;
+
+        System.out.println(a + d);
+    }
+}
