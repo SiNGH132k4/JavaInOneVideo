2b17088b14d58525a351f52dfcaf89f7a9fba2bc
Shivam Singh <ss132k4@gmail.com>
2025-03-26 23:27:56 +0530

    Adding Java in 1 video

---
+++ src/com/singh/javain1video/conditionals/Practice.java
@@ -0,0 +1,12 @@
+package com.singh.javain1video.conditionals;
+
+public class Practice {
+    public static void main(String[] args) {
+        boolean isSunny = true;
+        boolean isWarm = true;
+
+        if (isSunny && isWarm){
+            System.out.println("Beach day");
+        }
+    }
+}
--- src/com/singh/javain1video/datatypes/test.java
+++ src/com/singh/javain1video/datatypes/test.java
@@ -2,11 +2,31 @@ package com.singh.javain1video.datatypes;
 
 public class test {
     public static void main(String[] args) {
-        int a = 1;
-        byte b = 2;
-        short c = 3;
-        long d = 4;
 
-        System.out.println(a + d);
+        System.out.println(1 != 3);
+        // Relational Operators: < > <= >= == !=
+
+        boolean isRainy = true;
+        boolean isHumid = true;
+        boolean notGoOutside = isHumid || isRainy;
+        System.out.println(notGoOutside);
+        System.out.println(!isHumid);
+        // Logical Operators: && || !
+
+//        int a = 5; //101
+//        int b = 4; //100
+//                    // 100 = "AND operation(&)"
+//                    // 101 = "OR operation(|)"
+//                    // 001 = "XOR operation(^)"
+//                    // 010 = "NOT operation on 5(~)"
+//                    // 1010 = "Left Shift operator on 5(<<)"
+//                    // 010 = "Right Shift operator on 5(>>)"
+//
+//        System.out.println(Integer.toBinaryString(a & b));
+//        System.out.println(Integer.toBinaryString(a | b));
+//        System.out.println(Integer.toBinaryString(a ^ b));
+//        System.out.println(Integer.toBinaryString(~a));
+//        System.out.println(Integer.toBinaryString(a << 1));
+//        System.out.println(Integer.toBinaryString(a >> 1));
     }
 }
